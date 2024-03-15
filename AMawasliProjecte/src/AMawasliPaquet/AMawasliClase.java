63dbfb241029134b12e0e7a8cbf765e00e5a7ddf
convidat <convidat@daw1-28>
2024-03-15 10:24:19 -0400

    Commit para confirmar los cambios

---
+++ AMawasliProjecte/src/AMawasliPaquet/AMawasliClase.java
@@ -0,0 +1,11 @@
+package AMawasliPaquet;
+
+public class AMawasliClase {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Esto es un Mensaje para el examen de GitHub");
+
+	}
+
+}
